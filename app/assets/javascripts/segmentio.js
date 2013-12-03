7719d0b1f7106b4c7f4334809e5c51919cb9cf3f
Ben Black <benjyblack@gmail.com>
2013-12-03 21:28:34 +0000

    fix analytics

--- app/assets/javascripts/segmentio.js
+++ app/assets/javascripts/segmentio.js
@@ -1,16 +1,9 @@
-var analytics=analytics||[];(function(){var
-e=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page",
-"pageview","ab","alias","ready","group"],t=function(e){return
-function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var
-n=0;n<e.length;n++)analytics[e[n]]=t(e[n])})(),analytics.load=function(e){var
-t=document.createElement("script");t.type="text/javascript",t.async=!0,
-t.src=("https:"===document.location.protocol?"https://":"http://")+
-"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var
-n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)};
-analytics.load("urknpnzb78");
+window.analytics||(window.analytics=[]),window.analytics.methods=["identify","track","trackLink","trackForm","trackClick","trackSubmit","page","pageview","ab","alias","ready","group","on","once","off"],window.analytics.factory=function(a){return function(){var t=Array.prototype.slice.call(arguments);return t.unshift(a),window.analytics.push(t),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var method=window.analytics.methods[i];window.analytics[method]=window.analytics.factory(method)}window.analytics.load=function(a){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+a+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n)},window.analytics.SNIPPET_VERSION="2.0.6",
+window.analytics.load("urknpnzb78"),
+window.analytics.page();
 $(document).on('page:load', function() {
   console.log('page loaded');
-  analytics.pageview();
-  analytics.trackForm($('#new_visitor'), 'Signed Up');
-  analytics.trackForm($('#new_contact'), 'Contact Request');
+  window.analytics.pageview();
+  window.analytics.trackForm($('#new_visitor'), 'Signed Up');
+  window.analytics.trackForm($('#new_contact'), 'Contact Request');
 })
